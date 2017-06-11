b72823e4e25fc00d72ac8ccce36f4e14bfdb5904
OpalAndLight <no@example.com>
2017-06-11 17:56:37 +0200

    map with leaflet and static data source

---
+++ app/js/map/data-static.js
@@ -0,0 +1,92 @@
+module.exports = {
+    warehouses: [
+        {
+            id: 1,
+            name: "Main Warehouse",
+            type: "BUILDING",
+            location: [49.006837, 8.403578]
+        },
+        {
+            id: 2,
+            name: "Service Van 001",
+            type: "VEHICLE",
+            location: [49.011574, 8.387636]
+        },
+        {
+            id: 3,
+            name: "Service Van 002",
+            type: "VEHICLE",
+            location: [49.007999, 8.409891]
+        }
+    ],
+    serviceProduct: [
+        {
+            id: 1,
+            serialNumber: "KA-HSKA-C-1000001",
+            name: "E-Car",
+            description: "Basic City E-Car",
+            activationDate: "01.05.2017 14:00:00",
+            bom: [1,2,3,6],
+            documents: [1,2,3],
+            type: "CAR",
+            location: [49.010819, 8.417338]
+        },
+        {
+            id: 2,
+            serialNumber: "KA-HSKA-T-1000002",
+            name: "E-Truck M",
+            description: "Basic City E-Truck Mini 7,5T",
+            activationDate: "01.05.2017 14:00:00",
+            bom: [1,2,3,6],
+            documents: [1,2,3],
+            type: "TRUCK",
+            location: [48.992298, 8.402091]
+        },
+        {
+            id: 3,
+            serialNumber: "KA-HSKA-S-20001",
+            name: "Temparature Sensor",
+            description: "Temparature Sensor",
+            activationDate: "01.05.2017 14:00:00",
+            bom: null,
+            documents: [100],
+            type: "SENSOR",
+            location: [49.005426, 8.386955]
+        },
+    ],
+    serviceCenter: [
+        {
+            id: 1,
+            name: "KA-MAIN",
+            address: "",
+            phone: "",
+            mobile: "",
+            web: "",
+            email: "",
+            location: [49.009283, 8.403921],
+            technicians: [1, 2],
+        }
+    ],
+    customer: [
+        {
+            id: 1,
+            name: "HSKA",
+            location: [49.015373, 8.389502],
+            address: "HSKA",
+            phone: "",
+            mobile: "",
+            web: "",
+            email: ""
+        },
+        {
+            id: 2,
+            name: "POST",
+            location: [49.009098, 8.394233],
+            address: "POST",
+            phone: "",
+            mobile: "",
+            web: "",
+            email: ""
+        }
+    ],
+}
---
+++ app/js/map/index.js
@@ -0,0 +1,61 @@
+const data = require('./data-static.js')
+const iconSettings = require('./iconSettings.js')
+
+// return icons for marker with adapted settings
+function getIcon(category, type) {
+    let currentSetting = iconSettings.get(category, type)
+
+    return L.AwesomeMarkers.icon({
+        icon: currentSetting.icon,
+        prefix: 'fa',
+        markerColor: currentSetting.color
+    })
+}
+
+// sets all markers for certain category to current map
+function setMarkers(category) {
+    data[category].forEach((dataset) => {
+        L.marker(dataset.location, {icon: getIcon(category, dataset.type) })
+            .addTo(markers)
+            .bindPopup(`<b>${dataset.name}</b><br>
+                ${dataset.description ? 'description: ' + dataset.description + '<br>' : ''}
+                ${dataset.serialNumber ? 'serialNumber: ' + dataset.serialNumber + '<br>' : ''}
+                ${dataset.activationDate ? 'activationDate: ' + dataset.activationDate + '<br>' : ''}`);
+    })
+}
+
+// update displayed markers
+function updateMap(e) {
+    let inputs = [...document.getElementsByTagName('input')] // get all inputs into array
+
+    markers.clearLayers() // remove old markers
+
+    // set markers for all active categories
+    inputs.forEach((input) => {
+        if (input.checked) {
+            setMarkers(input.value)
+        }
+    })
+
+    // update map bounds if there are any markers
+    let markerBounds = markers.getBounds()
+    if (markerBounds.isValid()) {
+        mymap.fitBounds(markerBounds);
+    } else {
+        mymap.setView([49.009748, 8.403061], 13);
+    }
+}
+
+// update markers when form changes
+document.getElementById('mapForm').addEventListener("change", updateMap);
+
+// init map
+let mymap = L.map('mapid').setView([49.009748, 8.403061], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    id: 'mapbox.streets'
+}).addTo(mymap);
+
+// group for adding and removing all markers
+let markers = L.featureGroup()
+.addTo(mymap);

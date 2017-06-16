71523fb76211e5a2afaa609a02c14163f1eb8a46
Benjamin Roth <benni.roth91@gmail.com>
2017-06-16 18:58:19 +0200

    feature(service-completion): get result of part selection modal

--- app/js/service_completion/part-selection.js
+++ app/js/service_completion/part-selection.js
@@ -1,14 +1,17 @@
 const data = require('./data-parts-static.js');
 
 function displayParts() {
+    console.log('init partslist');
     let partsSelect = document.getElementById('parts-select');
     let partList = "";
     data.parts.forEach((part) => {
         console.log(part);
-        partList += `<option>${part.name}</option>`;
+        partList += `<option value="${part.id}">${part.name}</option>`;
     });
     console.log(partList);
     partsSelect.innerHTML = partList;
 }
+
+
 
 displayParts();

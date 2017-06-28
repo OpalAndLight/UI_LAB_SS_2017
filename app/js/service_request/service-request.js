f4f215dcc6813b3eb9d2b2e619aade1962adf78c
Marvin Rühe <marvin@server-fish.de>
2017-06-28 13:04:41 +0200

    Bootstrap style 🙁

--- app/js/service_request/service-request.js
+++ app/js/service_request/service-request.js
@@ -29,6 +29,8 @@ function showServiceRequest(the_data){
     selectField("properties_service_type", the_data.serviceType);
     selectField("properties_urgency", the_data.urgency);
     selectField("properties_type", the_data.type);
+
+    document.getElementById("customerDetails").href = "customer/customer3.html?" + cu.id;
 }
 
 function prepareForm(techs, sr){
@@ -45,8 +47,9 @@ function prepareForm(techs, sr){
     timeField.value = "1:30:00";
 
     let form = document.getElementById("service_request_form");
-    form.addEventListener("submit", () => {
-        
+    form.addEventListener("submit", (ev) => {  
+        ev.preventDefault();
+        ev.stopPropagation();
         let so_id = Math.floor(Math.random() * 1000); // Maybe this can be improved ;-)
         let so = {
             	id: so_id,
@@ -71,7 +74,7 @@ function prepareForm(techs, sr){
 function sendServiceOrder(so){
     // Do fancy things to service order and send to the central intelligence.
     alert("Your service was ordered!");
-    window.location = "index.html";
+    window.history.back();
 }
 
 let plannedParts = [ "Schrauben", "Eimer", "Nägel"];
@@ -84,9 +87,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let butt = document.getElementById("addPlannedPartsButton");
     butt.addEventListener("click", () => {
         let field = document.getElementById("addPlannedParts");
-        plannedParts.push(field.value);
-        renderPlannedParts();
-        field.value = "";
+        if (field.value.trim() !== ""){
+            plannedParts.push(field.value);
+            renderPlannedParts();
+            field.value = "";
+        }
     })
 });
 
